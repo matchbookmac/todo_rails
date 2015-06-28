9b04e3d7aa0afe6076e6368b063fc27cd449cfc0
Ian on MacBook Pro <imacd20@gmail.com>
2015-06-28 16:52:02 -0700

    Add done and not_done scopes to Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,7 @@ class Task < ActiveRecord::Base
   belongs_to :list
 
   validates :description, presence: true
+
+  scope :done, -> { where(done: true) }
+  scope :not_done, -> { where(done: false) }
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,4 +3,24 @@ require 'rails_helper'
 describe Task do
   it { should validate_presence_of :description }
   it { should belong_to :list }
+
+  describe '.done' do
+    it 'returns all done tasks' do
+      task = Task.new description: 'Do Laundry'
+      task.save
+      task.update done: true
+      expect(Task.done).to eq([task])
+    end
+  end
+
+  describe '.not_done' do
+    it 'returns all incomplete tasks' do
+      task_0 = Task.new description: 'Do Laundry'
+      task_0.save
+      task_0.update done: true
+      task_1 = Task.new description: 'Fold Laundry'
+      task_1.save
+      expect(Task.not_done).to eq([task_1])
+    end
+  end
 end
