0b3413c9c2a82bc3ec05dc5d820a487546294316
Ian on MacBook Pro <imacd20@gmail.com>
2015-06-28 17:33:02 -0700

    Add done method to list to return completed tasks for a list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,9 @@
 class List < ActiveRecord::Base
   has_many :tasks
-  validates :name, :presence => true
-  validates :description, :presence => true
+  validates :name, presence: true
+  validates :description, presence: true
+
+  def done
+    Task.where("list_id = ? AND done = ?", id, true)
+  end
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -4,4 +4,16 @@ describe List do
   it { should validate_presence_of :name }
   it { should validate_presence_of :description }
   it { should have_many :tasks }
+
+  describe '#done' do
+    it 'returns all done tasks for a list' do
+      list = List.new(name: "home", description: "Things to do at home")
+      task_0 = list.tasks.new description: 'Do Laundry'
+      task_0.save
+      task_1 = list.tasks.new description: 'Fold Laundry'
+      task_1.save
+      task_0.update(done: true)
+      expect(list.done).to eq([task_0])
+    end
+  end
 end
